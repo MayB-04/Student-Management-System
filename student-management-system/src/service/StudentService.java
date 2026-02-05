b72e64a00426bbf3563a57c7692812da7e6d473c
MayB-04 <maybadenhorst@gmail.com>
2026-02-05 18:14:16 +0200

    Adding services for student operations

--- student-management-system/src/service/StudentService.java
+++ student-management-system/src/service/StudentService.java
@@ -1,5 +1,23 @@
 package service;
 
+import java.util.List;
+import model.Student;
+import repository.StudentRepository;
+
 public class StudentService {
     
+    private StudentRepository repository = new StudentRepository();
+
+    public void addStudent(int id, String name, int age){
+        repository.addStudent(new Student(id,name,age));
+    }
+    public void removeStudent(int id){
+        repository.removeStudent(id);
+    }
+    public Student searchStudent(int id){
+        return repository.findStudentById(id);
+    }
+    public List<Student> listStudents(){
+        return repository.getAllStudents();
+    }
 }
