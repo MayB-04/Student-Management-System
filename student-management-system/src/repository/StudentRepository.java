e00ff7d2fbf13f06858b460a0ef00487317e3343
MayB-04 <maybadenhorst@gmail.com>
2026-02-05 18:08:13 +0200

    Student repositary for operations to do with the students

--- student-management-system/src/repository/StudentRepository.java
+++ student-management-system/src/repository/StudentRepository.java
@@ -1,5 +1,30 @@
 package repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Student;
+
 public class StudentRepository {
-    
+    private List<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+    public void removeStudent(int id){
+        students.removeIf(student -> student.getID() == id);
+    }
+
+    public Student findStudentById(int id){
+        for(Student individual : students){
+            if(individual.getID() == id){
+                return individual;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> getAllStudents(){
+        return students;
+    }
 }
